ec475ab33493fd15fbaca456aa99a0d3ab0e4661
lmxx1234567 <lmxx1234567@hotmail.com>
2020-11-26 20:13:15 +0800

    feat: get caseid

--- baker.py
+++ baker.py
@@ -5,14 +5,15 @@ from case_parsers import case_info
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Legal document parser')
-    parser.add_argument('attr',help='Which attributes to resolve',choices=['casename'])
-    parser.add_argument('filepath',help='Path to legal document')
+    parser.add_argument(
+        'attr', help='Which attributes to resolve', choices=['casename', 'caseid'])
+    parser.add_argument('filepath', help='Path to legal document')
     args = parser.parse_args()
-    lines=[]
+    lines = []
     with open(args.filepath) as f:
         lines = f.readlines()
-    
-    if args.attr=='casename':
+
+    if args.attr == 'casename':
         print(case_info.get_case_name(lines))
-    
-    
+    elif args.attr == 'caseid':
+        print(case_info.get_case_id(lines))
--- case_parsers/case_info.py
+++ case_parsers/case_info.py
@@ -1,4 +1,5 @@
 from . import schema
+import re
 
 
 def get_case_name(lines: [str]) -> str:
@@ -6,4 +7,12 @@ def get_case_name(lines: [str]) -> str:
         for trial_procedure in schema['properties']['trial_procedure']['enum']:
             if trial_procedure in line:
                 return line
+    return 'Not found'
+
+
+def get_case_id(lines: [str]) -> str:
+    for line in lines:
+        matchObj = re.search(r'[（|()]\d{4}[）|)].+号', line)
+        if matchObj is not None:
+            return matchObj.group()
     return 'Not found'
