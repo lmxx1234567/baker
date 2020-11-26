62814a972f952647a5d5d6e608d9f2caf19dea98
lmxx1234567 <lmxx1234567@hotmail.com>
2020-11-26 20:39:04 +0800

    feat: parse year

--- baker.py
+++ baker.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 import argparse
 import json
+
 from case_parsers import case_info
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Legal document parser')
     parser.add_argument(
-        'attr', help='Which attributes to resolve', choices=['casename', 'caseid'])
+        'attr', help='Which attributes to resolve', choices=['casename', 'caseid', 'year'])
     parser.add_argument('filepath', help='Path to legal document')
     args = parser.parse_args()
     lines = []
@@ -17,3 +18,5 @@ if __name__ == "__main__":
         print(case_info.get_case_name(lines))
     elif args.attr == 'caseid':
         print(case_info.get_case_id(lines))
+    elif 'year':
+        print(case_info.get_year(lines))
