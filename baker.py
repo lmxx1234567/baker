2b8d09334d4f053ed9d097d85a1dc3e8279d0415
lmxx1234567 <lmxx1234567@hotmail.com>
2020-11-29 23:58:15 +0800

    edit argument name

--- baker.py
+++ baker.py
@@ -7,16 +7,16 @@ from case_parsers import case_info
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Legal document parser')
     parser.add_argument(
-        'attr', help='Which attributes to resolve', choices=['casename', 'caseid', 'year', 'cause', 'trial_procedure'])
+        'attr', help='Which attributes to resolve', choices=['case_name', 'case_id', 'year', 'cause', 'trial_procedure'])
     parser.add_argument('filepath', help='Path to legal document')
     args = parser.parse_args()
     lines = []
     with open(args.filepath) as f:
         lines = f.readlines()
 
-    if args.attr == 'casename':
+    if args.attr == 'case_name':
         print(case_info.get_case_name(lines))
-    elif args.attr == 'caseid':
+    elif args.attr == 'case_id':
         print(case_info.get_case_id(lines))
     elif args.attr == 'year':
         print(case_info.get_year(lines))
