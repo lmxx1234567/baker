71cda34b06fa3b40e7e1a804f0a368f88460e685
lmxx1234567 <lmxx1234567@hotmail.com>
2020-12-13 15:46:53 +0800

    fix: support claims treat claims as controersies if no controersy found

--- data/training/generator.py
+++ data/training/generator.py
@@ -4,17 +4,18 @@ lines = []
 with open('case.txt') as f:
     lines = f.readlines()
 
+
 def loadField(fields):
     if type(fields) == dict:
-        for key,value in fields.items():
+        for key, value in fields.items():
             fields[key] = loadField(value)
         return fields
     elif type(fields) == list:
-        for index,value in enumerate(fields):
+        for index, value in enumerate(fields):
             fields[index] = loadField(value)
         return fields
     elif type(fields) == str and fields != '':
-        for lineNum,line in enumerate(lines):
+        for lineNum, line in enumerate(lines):
             index = line.find(fields)
             if index != -1:
                 return {
@@ -26,12 +27,10 @@ def loadField(fields):
         return fields
     else:
         return fields
-                
-    
-        
+
 
 with open('parsed_field.json') as f:
     fields = json.load(f)
     loadField(fields)
-    
-    print(json.dumps(fields,ensure_ascii=False))
+
+    print(json.dumps(fields, ensure_ascii=False))
