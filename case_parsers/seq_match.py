e9a8d701769a7689063c5b07e6fba9bf6c6e69c7
lmxx1234567 <lmxx1234567@hotmail.com>
2021-01-28 12:35:45 +0800

    fix: add connection pool for thread-safe

--- case_parsers/seq_match.py
+++ case_parsers/seq_match.py
@@ -1,16 +1,19 @@
-import http
 import json
-from http import client
 import case_parsers
+from urllib3 import HTTPConnectionPool
 
 failed_times = 0
+try:
+    pool = HTTPConnectionPool('127.0.0.1', port=8501, maxsize=32)
+except Exception as e:
+    print('Can not found sequence matching model!', e)
+    case_parsers.SEQ_MODEL_AVALIABLE = False
+
 
 def check_seq_match_model() -> bool:
     print('Checking sequence matching model...')
     try:
-        conn = client.HTTPConnection('127.0.0.1', 8501, timeout=1)
-        conn.request("GET", '/v1/models/seq_match_bert/metadata')
-        res = conn.getresponse()
+        res = pool.request("GET", '/v1/models/seq_match_bert/metadata')
         if res.status == 200:
             print('ok!')
             return True
@@ -19,12 +22,12 @@ def check_seq_match_model() -> bool:
             return False
     except Exception as e:
         print('Can not found sequence matching model!', e)
+        case_parsers.SEQ_MODEL_AVALIABLE = False
         return False
 
 
 def seq_match(con: str, cause: str) -> float:
     try:
-        conn = http.client.HTTPConnection('127.0.0.1', 8501, timeout=1)
         data = json.dumps({
             "inputs": {
                 "con_input": con,
@@ -32,14 +35,14 @@ def seq_match(con: str, cause: str) -> float:
             }
         })
         headers = {"Content-Type": "application/json"}
-        conn.request(
-            "POST", '/v1/models/seq_match_bert:predict', data, headers)
-        res = conn.getresponse()
-        res_json = json.load(res)
+        res = pool.request(
+            "POST", '/v1/models/seq_match_bert:predict', headers=headers, body=data)
+        res_json = json.loads(res.data.decode('utf-8'))
         return res_json['outputs'][0][0]
     except Exception as e:
+        global failed_times
         failed_times += 1
         print('seq_match error:', e)
-        if failed_times > 10:
+        if failed_times > 3:
             case_parsers.SEQ_MODEL_AVALIABLE = False
         return -1
